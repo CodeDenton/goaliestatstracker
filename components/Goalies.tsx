1605193495ffe9b0125c138ff1792c5b0194168e
Denton Manders <dentonmanders@gmail.com>
2026-01-20 19:24:24 -0800

    connected frontend to render backend

--- components/Goalies.tsx
+++ components/Goalies.tsx
@@ -15,7 +15,8 @@ interface GoalieType {
 }
 const Goalies = async () => {
   // http://127.0.0.1:8000/goalies/full
-  const res = await fetch(`http://127.0.0.1:8000/goalies/full`, {
+  const renderBackend = "https://goaliestatsfastapi.onrender.com/"
+  const res = await fetch(`${renderBackend}/goalies/full`, {
     cache: "no-store",
   });
 
